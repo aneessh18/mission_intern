2fb603b596167390730d3b554d2acda4dde24d9a
indavarapuaneesh <34550287+indavarapuaneesh@users.noreply.github.com>
2019-05-30 21:05:18 +0530

    Update lca_of_bst.cpp

--- lca_of_bst.cpp
+++ lca_of_bst.cpp
@@ -14,7 +14,7 @@ public:
         {
             if(root->val>=min(p->val,q->val)&&root->val<=max(p->val,q->val))
                 return root;
-            else if(root->val>min(p->val,q->val))
+            else if(root->val>max(p->val,q->val))
                 return lowestCommonAncestor(root->left,p,q);
             else
                 return lowestCommonAncestor(root->right,p,q);
